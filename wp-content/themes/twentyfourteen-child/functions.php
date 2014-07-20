c8a294f10c3374f6f308e6b2a4e21b50d3609d3a
Paul Cook <pbc25@case.edu>
2014-07-20 14:47:04 -0400

    actual header ish

--- wp-content/themes/twentyfourteen-child/functions.php
+++ wp-content/themes/twentyfourteen-child/functions.php
@@ -1,7 +1,7 @@
 <?php 
+add_action( 'get_header', 'set_need_help_cookie');
 function set_need_help_cookie() {
 	if (is_page('need-help')) {
-		setcookie("seencontact", "yes", time() + 30, "/"); 
+		setcookie("seencontact", "yes", time() + 2629740, "/");
 	}
 }
-add_action( 'init', 'set_need_help_cookie');
